03e85644af1cde8a7496155dd615c2444490847f
Alex Druta <drutasandu@gmail.com>
2020-11-16 13:21:43 +0200

    Initial Commit

---
+++ stori/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stori
+//
+//  Created by Alex on 16.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
