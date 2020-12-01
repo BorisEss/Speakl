cf08e32ef84ac45d73dcdae74870447f149d4054
Alex Druta <drutasandu@gmail.com>
2020-12-01 10:57:00 +0200

    - Updating missing files

---
+++ stori/Models/CurrentUser.swift
@@ -0,0 +1,23 @@
+//
+//  CurrentUser.swift
+//  stori
+//
+//  Created by Alex on 30.11.2020.
+//
+
+import Foundation
+
+struct CurrentUser: Decodable {
+    
+    static var shared: CurrentUser?
+    
+    var id: Int
+    var langToLearn: Int?
+    var nativeLang: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case nativeLang = "native_lang"
+        case langToLearn = "lang_to_learn"
+    }
+}
